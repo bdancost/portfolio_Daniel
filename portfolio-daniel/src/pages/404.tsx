b3c26996fb2088c866753b08d17899c0c4e2f884
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-25 23:37:23 -0300

    Responsividade, Ícones e Seções de Projeto

--- portfolio-daniel/src/pages/404.tsx
+++ portfolio-daniel/src/pages/404.tsx
@@ -1,5 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "500",
+});
 
 const NotFound = () => {
   return (
@@ -7,13 +13,19 @@ const NotFound = () => {
       <Head>
         <title>404 | Daniel</title>
       </Head>
-      <div>
-        <h1>404</h1>
-        <div>
-          <p>Page Not Found</p>
+      <div className="flex flex-col items-center text-center mt-12 md:mt-24 gap-8 md:gap-4 px-6 md:px-32 ">
+        <h1 className="text 5xl sm:text-7xl font-bold">404</h1>
+        <p className="flex flex-col gap-8 md:gap-4 md:text-xl">
+          <span>Oops, não conseguimos encontrar essa página!</span>
           <span>Clique no botão abaixo para voltar para a Página Inicial</span>
-        </div>
-        <Link href="/">Ir para Página Inicial</Link>
+        </p>
+
+        <Link
+          href="/"
+          className={`${roboto.className} p-4 bg-h-blue-500 rounded-xl text-black first-line:transition-all first-line:hover:bg-opacity-80 mt-5 md:mt-12 md:text-xl w-fit`}
+        >
+          Ir para Página Inicial
+        </Link>
       </div>
     </>
   );
--- portfolio-daniel/src/pages/contatos.tsx
+++ portfolio-daniel/src/pages/contatos.tsx
@@ -6,29 +6,39 @@ const Contatos = () => {
       <Head>
         <title>Contatos | Daniel</title>
       </Head>
-      <div>
-        <h1>Contatos</h1>
-        <ul>
-          <li>
-            <span>E-mail</span>
-            <div>
-              <a href="mailto:pr.danfc88@gmail.com">
+      <div className="mt-12 md:mt-24 space-y-8 md:space-y-16 px-6 md:px-32">
+        <h1 className="text-5xl md:text-7xl font-bold text-center">Contatos</h1>
+        <ul className="table mx-auto space-y-6 md:space-y-8">
+          <li className="md:text-xl">
+            <span className="font-bold">E-mail</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a
+                href="mailto:pr.danfc88@gmail.com"
+                target="_blank"
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+              >
                 daniel-pr.danfc88@gmail.com
               </a>
             </div>
           </li>
-          <li>
-            <span>LinkedIn</span>
-            <div>
-              <a href="https://www.linkedin.com/in/daniel-fernandes1988/">
+          <li className="md:text-xl">
+            <span className="font-bold">LinkedIn</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+                href="https://www.linkedin.com/in/daniel-fernandes1988/"
+              >
                 https://www.linkedin.com/in/daniel-fernandes1988/
               </a>
             </div>
           </li>
-          <li>
-            <span>GitHub</span>
-            <div>
-              <a href="https://github.com/bdancost">
+          <li className="md:text-xl">
+            <span className="font-bold">GitHub</span>
+            <div className="flex gap-1 md:gap-3 items-center">
+              <a
+                className="text-sm md:text-lg text-slate-300 underline truncate"
+                href="https://github.com/bdancost"
+              >
                 https://github.com/bdancost
               </a>
             </div>
